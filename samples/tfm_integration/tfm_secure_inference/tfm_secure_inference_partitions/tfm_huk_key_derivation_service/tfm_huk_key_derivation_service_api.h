9a2d3ae8bd12e9fd5794bc80cf9f2d0d081a90ec
Rajkumar Kanagaraj <rajkumar.kanagaraj@arm.com>
2022-01-29 00:03:55 +0000

    samples: tf-m: Add new cose cbor encode and sign service
    - Add the new "psa_huk_key_derivation_cose_cbor_encode_and_sign" ROT service to
    HUK key derivation secure partitions (added in manifest), previously cose cbor
    implementation is part of tflm service and also the private key is imported and
    handles created in tflm service which is not correct ideally these keys are
    created by HUK key derivation service, one of the commits already have changes
    in place to move private key creation and import as part of HUK key derivation
    secure partition service due to that cose is no longer able to access private
    key via key handle because of two different secure partition so moved
    cose_cbor implementation as a new service in HUK key derivation secure
    partition.
    - Remove TFM_HUK_KEY_DERIVATION_RSA_OEAP_KEY service as it is no longer
    required.
    - Update "psa_huk_key_derivation_ec_key" service API to get the psa key
    id argument, which is used by PSA API to import the private key on that
    particular ID.
    - Increased the stack size.
    
    Signed-off-by: Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>

--- samples/tfm_integration/tfm_secure_inference/tfm_secure_inference_partitions/tfm_huk_key_derivation_service/tfm_huk_key_derivation_service_api.h
+++ samples/tfm_integration/tfm_secure_inference/tfm_secure_inference_partitions/tfm_huk_key_derivation_service/tfm_huk_key_derivation_service_api.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Linaro Limited
+ * Copyright (c) 2021-2022 Linaro Limited
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,15 +9,43 @@
 #include "psa/client.h"
 #include "psa_manifest/sid.h"
 #include "psa/crypto.h"
+#include <stdbool.h>
+
+typedef struct {
+	psa_key_id_t key_id;
+	_Bool cbor_encode_and_sign_pld;
+	size_t max_buf_size;
+} cose_cbor_config_t;
 
 /**
  * \brief Generate EC Key
  *
  * Generates an EC Key
  *
- * \param[in] key_id          Key slot id for persistent key
+ * \param[in] ec_key_id         EC key id for persistent key
+ * \param[in] label             Unique label string for the key derivation seed value
+ * \param[in] label_size        Unique label string size
  *
  * \return A status indicating the success/failure of the operation
  */
-psa_status_t psa_huk_key_derivation_ec_key(uint8_t *ec_priv_key_data,
-					   size_t ec_priv_key_data_size);
+
+psa_status_t psa_huk_key_derivation_ec_key(psa_key_id_t *ec_key_id,
+					   const uint8_t *label, size_t label_size);
+
+/**
+ * \brief COSE CBOR encode and sign
+ *
+ * COSE CBOR encode and sign
+ *
+ * \param[in]  inf_value        Tflm inference value to encode and sign
+ * \param[in]  cfg              Pointer to COSE CBOR config
+ * \param[out] encoded_buf      Buffer to which encoded data
+ *                              is written into
+ * \param[out] encoded_buf_len  Encoded buffer len in bytes
+ *
+ * \return A status indicating the success/failure of the operation
+ */
+psa_status_t psa_huk_key_derivation_cose_cbor_encode_and_sign(float *inf_value,
+							      cose_cbor_config_t *cfg,
+							      uint8_t *encoded_buf,
+							      size_t *encoded_buf_len);
