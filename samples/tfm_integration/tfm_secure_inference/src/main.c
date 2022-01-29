016cb966fff21bb46b0e5e0e5d8f68ff3a06c2c3
Rajkumar Kanagaraj <rajkumar.kanagaraj@arm.com>
2022-01-29 00:07:38 +0000

    samples: tf-m: Update the inference main application on NS-side
    - Update the main to get the C_SIGN public key from HUK key derivation export
    public key service on S-side.
    - Update to pass C_SIGN key id to psa secure inference tflm hello function
    argument.
    - Update the decode and verify sign function to run on lively received encode
    and signed payload from the S-side.
    - Update decode and verify sign function to call cose decoder on NS-side to
    retrieve the inference value.
    
    Signed-off-by: Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>

--- samples/tfm_integration/tfm_secure_inference/src/main.c
+++ samples/tfm_integration/tfm_secure_inference/src/main.c
@@ -12,40 +12,12 @@
 
 #include <tfm_veneers.h>
 #include <tfm_ns_interface.h>
-#include "tfm_tflm_service_api.h"
+#include "tfm_secure_infer_partitions_service_api.h"
 #include "cose_verify.h"
 #include "util_app_log.h"
+#include "util_sformat.h"
 #include "pk_import_verify_sign.h"
 #include <math.h>
-
-// TODO: Replace with getting the public key from S-side
-const uint8_t temp_ecdsaPublic[66] = {
-	0x4, 0x18, 0x4d, 0xc2, 0x5c, 0xb, 0x32,
-	0x2f, 0xfb, 0xff, 0xd, 0xdf, 0x9b, 0x55,
-	0x87, 0x32, 0xf3, 0x53, 0xf8, 0x9a, 0xf1,
-	0x1b, 0x1c, 0x89, 0x3a, 0x8f, 0xd5, 0xb1,
-	0x4d, 0x9d, 0x5a, 0xed, 0x8e, 0x92, 0xea,
-	0xda, 0x95, 0x24, 0xdf, 0xd4, 0xcc, 0xcc,
-	0x4b, 0xe3, 0x3c, 0x1, 0xc8, 0x2c, 0xb3,
-	0xbf, 0xb9, 0x21, 0x68, 0x71, 0x5a, 0x5b,
-	0xbc, 0xc4, 0xa, 0x24, 0x9d, 0x74, 0xad,
-	0xc, 0x68
-};
-
-// TODO: Replace with getting cose payload from S-side
-uint8_t cose_encoded_payload[256] = {
-	0xd2, 0x84, 0x43, 0xa1, 0x1, 0x26, 0xa0, 0x4b,
-	0xa1, 0x3a, 0x0, 0x1, 0x38, 0x7f, 0x44, 0x1f,
-	0x85, 0xab, 0x3f, 0x58, 0x40, 0xb7, 0x61, 0x7c,
-	0x38, 0x29, 0x4b, 0xe, 0x78, 0xbf, 0x92, 0xb5,
-	0x93, 0x74, 0x9c, 0x6c, 0x40, 0x72, 0x13, 0x71,
-	0xb0, 0x6a, 0x8a, 0x2, 0x49, 0x4f, 0xa4, 0xad,
-	0x7b, 0x15, 0x8, 0x10, 0x4a, 0x37, 0xc6, 0x26,
-	0x17, 0x31, 0xee, 0xcf, 0x60, 0x89, 0xa7, 0xfc,
-	0x46, 0x71, 0xfd, 0x6e, 0xe1, 0x63, 0xe5, 0x13,
-	0x33, 0xcb, 0x57, 0x2f, 0x7e, 0x75, 0x75, 0x1a,
-	0x25, 0xc1, 0xd2, 0x75, 0xd6
-};
 
 /** Declare a reference to the application logging interface. */
 LOG_MODULE_DECLARE(app, CONFIG_LOG_DEFAULT_LEVEL);
@@ -61,8 +33,8 @@ int tflm_inference_value_decode_and_verify_sign(uint8_t *inf_val_encoded_buf,
 	int status;
 
 	status = mbedtls_ecp_load_representation(&ctx.pk,
-						 pkey, strlen(pkey)
-						 );
+						 pkey,
+						 strlen(pkey));
 	if (status != 0) {
 		LOG_ERR("Load the public key failed\n");
 		goto err;
@@ -71,8 +43,8 @@ int tflm_inference_value_decode_and_verify_sign(uint8_t *inf_val_encoded_buf,
 	status = cose_sign_init(&ctx);
 	if (status != COSE_ERROR_NONE) {
 		LOG_ERR("Failed to initialize COSE signing context.\n");
+		goto err;
 	}
-	goto err;
 
 	status = cose_verify_sign1(&ctx,
 				   inf_val_encoded_buf,
@@ -84,11 +56,11 @@ int tflm_inference_value_decode_and_verify_sign(uint8_t *inf_val_encoded_buf,
 		goto err;
 	}
 
-	// Print the decoded payload
-	for (int i = 0; i < len_dec; i++)
-		LOG_INF("%x ", dec[i]);
-	LOG_INF("\n");
-
+	status = cose_payload_decode(dec, len_dec, y_val);
+	if (status != COSE_ERROR_NONE) {
+		LOG_ERR("Failed to authenticate signature.\n");
+		goto err;
+	}
 	return status;
 err:
 	al_dump_log();
@@ -103,34 +75,63 @@ void main(void)
 	const float PI = 3.14159265359f;
 	float deg = PI / 180.0;
 
+	psa_key_id_t key_id = C_SIGN; // Using Device COSE SIGN key
+	uint8_t inf_val_encoded_buf[256];
+	size_t inf_val_encoded_buf_len = 0;
+	uint8_t public_key[65] = { 0 }; /* EC public key = 65 bytes. */
+	size_t public_key_len = sizeof(public_key);
 	float x_value, y_value;
 
 	/* Initialise the logger subsys and dump the current buffer. */
 	log_init();
 
-	if (tflm_inference_value_decode_and_verify_sign(&cose_encoded_payload[0],
-							sizeof(cose_encoded_payload),
-							temp_ecdsaPublic,
-							&y_value) != 0) {
-		LOG_ERR("Failed to verify signature.\n");
-	} else {
-		LOG_INF("Verified the signature using the public key.\n");
+	/* String format output config. */
+	struct sf_hex_tbl_fmt fmt = {
+		.ascii = true,
+		.addr_label = true,
+		.addr = 0
+	};
+
+	status = al_psa_status(
+		psa_huk_key_derivation_export_public_key(&key_id,
+							 public_key,
+							 public_key_len),
+		__func__);
+	if (status != PSA_SUCCESS) {
+		LOG_ERR("Failed to export the_public_key");
+		goto err;
 	}
 
-	for (int i = 0; i <= 20; i++) {
+	for (int i = 1; i <= 20; i++) {
 
 		x_value = (float)i * deg;
 		status = al_psa_status(
-			psa_secure_inference_tflm_hello(&x_value,
+			psa_secure_inference_tflm_hello(&key_id,
+							&x_value,
 							sizeof(x_value),
-							&y_value,
-							sizeof(y_value)),
+							inf_val_encoded_buf,
+							sizeof(inf_val_encoded_buf),
+							&inf_val_encoded_buf_len),
 			__func__);
 
 		if (status != PSA_SUCCESS) {
 			LOG_ERR("Failed to get sine value using secure inference");
 			goto err;
 		}
+
+		LOG_INF("CBOR encoded and COSE signed inference value:");
+		sf_hex_tabulate_16(&fmt, inf_val_encoded_buf, inf_val_encoded_buf_len);
+
+		if (tflm_inference_value_decode_and_verify_sign(
+			    &inf_val_encoded_buf[0],
+			    inf_val_encoded_buf_len,
+			    public_key,
+			    &y_value) != 0) {
+			LOG_ERR("NS: Failed to verify signature.\n");
+		} else {
+			LOG_INF("NS: Verified the signature using the public key.\n");
+		}
+
 
 		printf("Model: Sine of %d deg is: %f\t", i, y_value);
 		printf("C Mathlib: Sine of %d deg is: %f\t", i, sin(x_value));
