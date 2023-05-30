9a0502793de196cc0caccd9c81c0a94cd7efb92a
Sadik.Ozer <sadik.ozer@analog.com>
2023-05-30 15:39:59 +0300

    Rename fthr2 folder as fthr and add fthr config files
    Signed-off-by: Sadik.Ozer <sadik.ozer@analog.com>

--- boards/arm/max32666fthr/board.cmake
+++ boards/arm/max32666fthr/board.cmake
@@ -1,7 +1,9 @@
 # Copyright (c) 2023 Analog Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+if(CONFIG_BOARD_MAX32666FTHR OR CONFIG_BOARD_MAX32666FTHR2)
 board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
 board_runner_args(openocd --cmd-pre-init "source [find target/max32665.cfg]")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
