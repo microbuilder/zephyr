01c2186689b01611e4067880b8326b8d5f81a616
Sadik.Ozer <sadik.ozer@analog.com>
2023-06-02 17:27:26 +0300

    Update gpio driver to use SDK files, not finalized
    Signed-off-by: Sadik.Ozer <sadik.ozer@analog.com>

--- drivers/gpio/gpio_max32.c
+++ drivers/gpio/gpio_max32.c
@@ -10,54 +10,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include <gpio.h>
+
 #define DT_DRV_COMPAT adi_max32_gpio
 
 LOG_MODULE_REGISTER(gpio_max32, CONFIG_GPIO_LOG_LEVEL);
 
-struct max32_gpio_regs {
-	uint32_t en0;
-	uint32_t en0_set;
-	uint32_t en0_clr;
-	uint32_t outen;
-	uint32_t outen_set;
-	uint32_t outen_clr;
-	uint32_t out;
-	uint32_t out_set;
-	uint32_t out_clr;
-	uint32_t in;
-	uint32_t intmode;
-	uint32_t intpol;
-	uint32_t inen;
-	uint32_t inten;
-	uint32_t inten_set;
-	uint32_t inten_clr;
-	uint32_t intfl;
-	uint32_t rsv_0x44;
-	uint32_t intfl_clr;
-	uint32_t wken;
-	uint32_t wken_set;
-	uint32_t wken_clr;
-	uint32_t rsv_0x58;
-	uint32_t dualedge;
-	uint32_t padctrl0;
-	uint32_t padctrl1;
-	uint32_t en1;
-	uint32_t en1_set;
-	uint32_t en1_clr;
-	uint32_t en2;
-	uint32_t en2_set;
-	uint32_t en2_clr;
-	uint32_t en3;
-	uint32_t en3_set;
-	uint32_t en3_clr;
-	uint32_t hysen;
-	uint32_t srsel;
-	uint32_t ds0;
-	uint32_t ds1;
-	uint32_t ps;
-	uint32_t rsv_0xbc;
-	uint32_t vssel;
-};
 struct gpio_max32_data {
 	struct gpio_driver_data common;
 	sys_slist_t cb_list;
@@ -65,7 +23,7 @@ struct gpio_max32_data {
 
 struct gpio_max32_config {
 	struct gpio_driver_config common;
-	volatile struct max32_gpio_regs *gpio;
+	volatile mxc_gpio_regs_t *gpio;
 	const struct pinctrl_dev_config *pctrl;
 	const struct device *clock;
 	void (*irq_func)(void);
@@ -76,9 +34,7 @@ struct gpio_max32_config {
 static int gpio_max32_port_get_raw(const struct device *dev, uint32_t *value)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-
-	*value = cfg->gpio->in;
-
+	*value = MXC_GPIO_InGet((mxc_gpio_regs_t *)(cfg->gpio), (unsigned int)-1);
 	return 0;
 }
 
@@ -86,47 +42,35 @@ static int gpio_max32_port_set_masked_raw(const struct device *dev, gpio_port_pi
 					  gpio_port_value_t value)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-
-	if (value) {
-		cfg->gpio->out_set = mask;
-	} else {
-		cfg->gpio->out_clr = mask;
-	}
-
+	MXC_GPIO_OutPut((mxc_gpio_regs_t *)(cfg->gpio), mask, value);
 	return 0;
 }
 
 static int gpio_max32_port_set_bits_raw(const struct device *dev, gpio_port_pins_t pins)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-
-	cfg->gpio->out_set = pins;
-
+	MXC_GPIO_OutSet((mxc_gpio_regs_t *)(cfg->gpio), pins);
 	return 0;
 }
 
 static int gpio_max32_port_clear_bits_raw(const struct device *dev, gpio_port_pins_t pins)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-
-	cfg->gpio->out_clr = pins;
-
+	MXC_GPIO_OutClr((mxc_gpio_regs_t *)(cfg->gpio), pins);
 	return 0;
 }
 
 static int gpio_max32_port_toggle_bits(const struct device *dev, gpio_port_pins_t pins)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-
-	cfg->gpio->out ^= pins;
-
+	MXC_GPIO_OutToggle((mxc_gpio_regs_t *)(cfg->gpio), pins);
 	return 0;
 }
 
 int gpio_max32_config_pinmux(const struct device *dev, int pin, int pinmux, int pincfg)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-	volatile struct max32_gpio_regs *gpio = cfg->gpio;
+	mxc_gpio_regs_t *gpio = (mxc_gpio_regs_t *)cfg->gpio;
 
 	int mode = MAX32_PINMUX_MODE(pinmux);
 
@@ -134,22 +78,22 @@ int gpio_max32_config_pinmux(const struct device *dev, int pin, int pinmux, int 
 	case MAX32_GPIO:
 		break;
 	case MAX32_AF1:
-		gpio->en0_clr = BIT(pin);
+		gpio->en_clr = BIT(pin);
 		gpio->en1_clr = BIT(pin);
 		gpio->en2_clr = BIT(pin);
 		break;
 	case MAX32_AF2:
-		gpio->en0_clr = BIT(pin);
+		gpio->en_clr = BIT(pin);
 		gpio->en1_set = BIT(pin);
 		gpio->en2_clr = BIT(pin);
 		break;
 	case MAX32_AF3:
-		gpio->en0_clr = BIT(pin);
+		gpio->en_clr = BIT(pin);
 		gpio->en1_clr = BIT(pin);
 		gpio->en2_set = BIT(pin);
 		break;
 	case MAX32_AF4:
-		gpio->en0_clr = BIT(pin);
+		gpio->en_clr = BIT(pin);
 		gpio->en1_set = BIT(pin);
 		gpio->en2_set = BIT(pin);
 		break;
@@ -158,20 +102,20 @@ int gpio_max32_config_pinmux(const struct device *dev, int pin, int pinmux, int 
 	}
 
 	if (pincfg & BIT(MAX32_INPUT_ENABLE_SHIFT)) {
-		gpio->inen |= BIT(pin);
+		gpio->in_en |= BIT(pin);
 	}
 
 	if (pincfg & BIT(MAX32_OUTPUT_ENABLE_SHIFT)) {
-		gpio->outen_set = BIT(pin);
+		gpio->out_en_set = BIT(pin);
 	}
 
 	/* select strong pull */
 	gpio->ps |= BIT(pin);
 
 	if (pincfg & BIT(MAX32_BIAS_PULL_UP_SHIFT)) {
-		gpio->padctrl0 |= BIT(pin);
+		gpio->pad_cfg1 |= BIT(pin);
 	} else if (pincfg & BIT(MAX32_BIAS_PULL_DOWN_SHIFT)) {
-		gpio->padctrl1 |= BIT(pin);
+		gpio->pad_cfg2 |= BIT(pin);
 	}
 
 	if (pincfg & BIT(MAX32_OUTPUT_HIGH_SHIFT)) {
@@ -188,18 +132,18 @@ int gpio_max32_config_pinmux(const struct device *dev, int pin, int pinmux, int 
 static int gpio_max32_config(const struct device *dev, gpio_pin_t pin, gpio_flags_t flags)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-	volatile struct max32_gpio_regs *gpio = cfg->gpio;
+	mxc_gpio_regs_t *gpio = (mxc_gpio_regs_t *)cfg->gpio;
 
 	if (flags & GPIO_INPUT) {
-		gpio->inen |= BIT(pin);
+		gpio->in_en |= BIT(pin);
 	} else {
-		gpio->inen &= ~BIT(pin);
+		gpio->in_en &= ~BIT(pin);
 	}
 
 	if (flags & GPIO_OUTPUT) {
-		gpio->outen_set = BIT(pin);
+		gpio->out_en_set = BIT(pin);
 	} else {
-		gpio->outen_clr = BIT(pin);
+		gpio->out_en_clr = BIT(pin);
 	}
 
 	if (flags & GPIO_OUTPUT_INIT_LOW) {
@@ -215,19 +159,19 @@ static int gpio_max32_pin_interrupt_configure(const struct device *dev, gpio_pin
 					      enum gpio_int_mode mode, enum gpio_int_trig trig)
 {
 	const struct gpio_max32_config *cfg = dev->config;
-	volatile struct max32_gpio_regs *gpio = cfg->gpio;
+	volatile mxc_gpio_regs_t *gpio = cfg->gpio;
 
 	if (mode == GPIO_INT_MODE_DISABLED) {
-		gpio->inten_clr = BIT(pin);
+		gpio->int_en_clr = BIT(pin);
 		return 0;
 	}
 
 	switch (trig) {
 	case GPIO_INT_TRIG_LOW:
-		gpio->intpol &= ~BIT(pin);
+		gpio->int_pol &= ~BIT(pin);
 		break;
 	case GPIO_INT_TRIG_HIGH:
-		gpio->intpol |= BIT(pin);
+		gpio->int_pol |= BIT(pin);
 		break;
 	case GPIO_INT_TRIG_BOTH:
 		return -ENOTSUP;
@@ -237,16 +181,16 @@ static int gpio_max32_pin_interrupt_configure(const struct device *dev, gpio_pin
 
 	switch (mode) {
 	case GPIO_INT_MODE_LEVEL:
-		gpio->intmode &= ~BIT(pin);
+		gpio->int_mod &= ~BIT(pin);
 		break;
 	case GPIO_INT_MODE_EDGE:
-		gpio->intmode |= BIT(pin);
+		gpio->int_mod |= BIT(pin);
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	gpio->inten_set = BIT(pin);
+	gpio->int_en_set = BIT(pin);
 
 	return 0;
 }
@@ -276,9 +220,9 @@ static void gpio_max32_isr(const void *param)
 	const struct gpio_max32_config *cfg = dev->config;
 	struct gpio_max32_data *data = dev->data;
 
-	uint32_t pins = cfg->gpio->intfl;
+	uint32_t pins = cfg->gpio->int_stat;
 
-	cfg->gpio->intfl_clr = pins;
+	cfg->gpio->int_clr = pins;
 
 	gpio_fire_callbacks(&data->cb_list, dev, pins);
 }
@@ -316,7 +260,7 @@ static int gpio_max32_init(const struct device *dev)
 	}                                                                                          \
 	static struct gpio_max32_data gpio_max32_data_##n;                                         \
 	static const struct gpio_max32_config gpio_max32_config_##n = {                            \
-		.gpio = (struct max32_gpio_regs *)DT_INST_REG_ADDR(n),                             \
+		.gpio = (mxc_gpio_regs_t *)DT_INST_REG_ADDR(n),                                    \
 		.clock = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(n)),                                    \
 		.clock_bus = DT_INST_CLOCKS_CELL(n, offset),                                       \
 		.clock_bit = DT_INST_CLOCKS_CELL(n, bit),                                          \
