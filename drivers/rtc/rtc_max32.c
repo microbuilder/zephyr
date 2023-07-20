65f19889121798fb6c7a9e4f2e727dea2a342e98
oknshn <okan.sahin@analog.com>
2023-07-20 16:17:37 +0300

    drivers: rtc: Add CONFIG_RTC_ALARM support
    Irq has not been connected yet.
    
    Signed-off-by: oknshn <okan.sahin@analog.com>

--- drivers/rtc/rtc_max32.c
+++ drivers/rtc/rtc_max32.c
@@ -31,6 +31,7 @@ struct rtc_max32_data {
 	void *cb_data;
 	rtc_update_callback update_cb;
 	void *update_cb_data;
+	struct rtc_time alarm_time;
 };
 
 static int rtc_max32_set_time(const struct device *dev, const struct rtc_time *timeptr)
@@ -66,14 +67,120 @@ static int rtc_max32_get_time(const struct device *dev, struct rtc_time *timeptr
 	return 0;
 }
 
+#if defined(CONFIG_RTC_ALARM)
+static int rtc_max32_alarm_get_supported_fields(const struct device *dev, uint16_t id,
+						uint16_t *mask)
+{
+	struct rtc_max32_data * const dev_data = dev->data;
+
+	if (dev_data->alarms_count <= id) {
+		return -EINVAL;
+	}
+
+	(*mask) = (RTC_ALARM_TIME_MASK_SECOND
+		   | RTC_ALARM_TIME_MASK_MINUTE
+		   | RTC_ALARM_TIME_MASK_HOUR
+		   | RTC_ALARM_TIME_MASK_MONTHDAY
+		   | RTC_ALARM_TIME_MASK_MONTH);
+
+	return 0;
+}
+
+static int rtc_max32_alarm_set_time(const struct device *dev, uint16_t id, uint16_t mask,
+				   const struct rtc_time *timeptr)
+{
+	struct rtc_max32_data * const dev_data = dev->data;
+
+	if (dev_data->alarms_count <= id) {
+		return -EINVAL;
+	}
+
+	if ((mask > 0) && (timeptr == NULL)) {
+		return -EINVAL;
+	}
+
+	dev_data->mask = mask;
+	dev_data->alarm_time = *timeptr;
+
+	struct rtc_time *now_time;
+	struct time_t now_secs, secs, diff_secs;
+
+	rtc_max32_get_time(dev, now_time);
+	gmtime_r((time_t *)&now_secs, (struct tm *)(now_time));
+	gmtime_r((time_t *)&secs, (struct tm *)(timeptr));
+
+	diff_secs = difftime(secs, now_secs);
+
+	MXC_RTC_SetTimeofdayAlarm((uint32_t)diff_secs);
+
+	return 0;
+}
+
+static int rtc_max32_alarm_get_time(const struct device *dev, uint16_t id, uint16_t *mask,
+				   struct rtc_time *timeptr)
+{
+	struct rtc_max32_data * const dev_data = dev->data;
+
+	if (dev_data->alarms_count <= id) {
+		return -EINVAL;
+	}
+
+	if (timeptr == NULL) {
+		return -EINVAL
+	}
+
+	*timeptr = dev_data->alarm_time;
+	*mask = dev_data->mask;
+
+	return 0;
+}
+
+static int rtc_max32_alarm_is_pending(const struct device *dev, uint16_t id)
+{
+	struct rtc_max32_data * const dev_data = dev->data;
+	int ret;
+
+	if (dev_data->alarms_count <= id) {
+		return -EINVAL;
+	}
+
+	ret = (dev_data->alarm_pending == true) ? 1 : 0;
+	dev_data->alarm_pending = false;
+
+	return ret;
+}
+#endif /* CONFIG_RTC_ALARM */
+
+static void rtc_max32_isr(const struct device *dev)
+{
+	struct rtc_max32_data * const dev_data = dev->data;
+
+	ARG_UNUSED(dev_data);
+
+#if defined(CONFIG_RTC_ALARM)
+	if (!MXC_RTC_RevA_GetBusyFlag()) {
+		if (dev_data->cb) {
+			dev_data->cb(dev, 0, dev_data->cb_data);
+		}
+		dev_data->alarm_pending = false;
+	}
+#endif
+
+}
+
 struct rtc_driver_api rtc_max32_driver_api = {
 	.set_time = rtc_max32_set_time,
 	.get_time = rtc_max32_get_time,
+#if defined(CONFIG_RTC_ALARM)
+	.alarm_get_supported_fields = rtc_max32_alarm_get_supported_fields,
+	.alarm_set_time = rtc_max32_alarm_set_time,
+	.alarm_get_time = rtc_max32_alarm_get_time,
+	.alarm_is_pending = rtc_max32_alarm_is_pending,
+#endif /* CONFIG_RTC_ALARM */
 };
 
 static int rtc_max32_init(const struct device *dev)
 {
-	ARG_UNUSED(dev);
 	/* Set time to initial time */
 	return MXC_RTC_Init(0, 0);
 }
