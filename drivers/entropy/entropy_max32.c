c00a657a83a2cb4d90fdf196b68ed90e4540f8a6
oknshn <okan.sahin@analog.com>
2023-08-08 18:23:20 +0300

    Add trng support for MAX32xxx

---
+++ drivers/entropy/entropy_max32.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2018 Aurelien Jarno
+ * Copyright (c) 2023 Gerson Fernando Budke
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "zephyr/sys/util.h"
+
+#define DT_DRV_COMPAT adi_max32_trng
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/entropy.h>
+#include <errno.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <soc.h>
+#include <string.h>
+
+#include "trng.h"
+
+struct trng_max32_cfg {
+	mxc_trng_regs_t *regs;
+};
+
+static int entropy_max32_get_entropy(const struct device *dev, uint8_t *buffer,
+				   uint16_t length)
+{
+        return MXC_TRNG_Random(buffer, length);
+}
+
+static int entropy_max32_get_entropy_isr(const struct device *dev,
+				       uint8_t *buffer,
+				       uint16_t length, uint32_t flags)
+{
+	return 0;
+}
+
+static int entropy_max32_init(const struct device *dev)
+{
+
+	return MXC_TRNG_Init();
+}
+
+static const struct entropy_driver_api entropy_max32_api = {
+	.get_entropy = entropy_max32_get_entropy,
+	.get_entropy_isr = entropy_max32_get_entropy_isr
+};
+
+static const struct trng_max32_cfg trng_max32_cfg = {
+	.regs = (mxc_trng_regs_t *)DT_INST_REG_ADDR(0),
+};
+
+DEVICE_DT_INST_DEFINE(0,
+		    entropy_max32_init, NULL,
+		    NULL, &trng_max32_cfg,
+		    PRE_KERNEL_1, CONFIG_ENTROPY_INIT_PRIORITY,
+		    &entropy_max32_api);
