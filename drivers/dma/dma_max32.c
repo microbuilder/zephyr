f161fbcd026f69b46cefe2fcbf789bd013f403b9
Jason Murphy <jason.murphy@analog.com>
2023-08-08 15:31:29 +0100

    drivers: dma: Configured DMA ISR callback for MAX32 series
    Signed-off-by: Jason Murphy <jason.murphy@analog.com>

--- drivers/dma/dma_max32.c
+++ drivers/dma/dma_max32.c
@@ -17,6 +17,20 @@
 
 LOG_MODULE_REGISTER(max32_dma, CONFIG_DMA_LOG_LEVEL);
 
+struct max32_dma_config {
+	mxc_dma_regs_t *regs;
+	const struct device *clock;
+	struct max32_perclk perclk;
+    uint8_t channels;
+};
+
+struct max32_dma_data {
+    dma_callback_t callback;
+    void *cb_data;
+};
+
+static struct max32_dma_data dma_data[DT_PROP(DT_NODELABEL(dma), dma_channels)];
+
 static int is_valid_dma_width(uint32_t width)
 {
     int valid = 0;
@@ -54,12 +68,6 @@ static mxc_dma_width_t dma_width_z_to_mxc(uint32_t width)
     }
 }
 
-struct max32_dma_config {
-	mxc_dma_regs_t *regs;
-	const struct device *clock;
-	struct max32_perclk perclk;
-};
-
 /*
  * APIs
  */
@@ -69,37 +77,44 @@ static inline int max32_dma_config(const struct device *dev,
 					     struct dma_config *config)
 {
     int ret = 0;
+    const struct max32_dma_config *cfg = dev->config;
+    struct max32_dma_data *data = dev->data;
+
+    if (channel >= cfg->channels) {
+		LOG_ERR("Invalid DMA channel - must be < %" PRIu32 " (%" PRIu32 ")", cfg->channels, channel);
+		return -EINVAL;
+	}
 
     /* DMA Channel Config */
-    mxc_dma_config_t dma_cfg;
-    dma_cfg.ch = channel;
-    dma_cfg.reqsel = MXC_DMA_REQUEST_MEMTOMEM; // initial test
+    mxc_dma_config_t mxc_dma_cfg;
+    mxc_dma_cfg.ch = channel;
+    mxc_dma_cfg.reqsel = MXC_DMA_REQUEST_MEMTOMEM; // initial test
     if (!(is_valid_dma_width(config->source_data_size) \
                         || !(is_valid_dma_width(config->dest_data_size))))
     {
         LOG_ERR("Invalid DMA width - must be byte (1), halfword (2) or word (4) !");
-        return -ENOTSUP;
+        return -EINVAL;
 	}
-    dma_cfg.srcwd = dma_width_z_to_mxc(config->source_data_size);
-    dma_cfg.dstwd = dma_width_z_to_mxc(config->dest_data_size);
-    // dma_cfg.srcinc_en = config->source_chaining_en;
-    // dma_cfg.dstinc_en = config->dest_chaining_en;
-    dma_cfg.srcinc_en = 1;
-    dma_cfg.dstinc_en = 1;
+    mxc_dma_cfg.srcwd = dma_width_z_to_mxc(config->source_data_size);
+    mxc_dma_cfg.dstwd = dma_width_z_to_mxc(config->dest_data_size);
+    // mxc_dma_cfg.srcinc_en = config->source_chaining_en;
+    // mxc_dma_cfg.dstinc_en = config->dest_chaining_en;
+    mxc_dma_cfg.srcinc_en = 1;
+    mxc_dma_cfg.dstinc_en = 1;
 
     /* DMA Channel Advanced Config */
-    mxc_dma_adv_config_t dma_cfg_adv;
-    dma_cfg_adv.ch = channel;
+    mxc_dma_adv_config_t mxc_dma_cfg_adv;
+    mxc_dma_cfg_adv.ch = channel;
     if (!is_valid_dma_ch_prio(config->channel_priority))
     {
         LOG_ERR("Invalid DMA priority - must comply with type mxc_dma_priority_t (0 - 3)");
-        return -ENOTSUP;
+        return -EINVAL;
     }
-    dma_cfg_adv.prio = config->channel_priority;
-    dma_cfg_adv.reqwait_en = 0;
-    dma_cfg_adv.tosel = MXC_DMA_TIMEOUT_4_CLK;
-    dma_cfg_adv.pssel = MXC_DMA_PRESCALE_DISABLE;
-    dma_cfg_adv.burst_size = config->source_burst_length;
+    mxc_dma_cfg_adv.prio = config->channel_priority;
+    mxc_dma_cfg_adv.reqwait_en = 0;
+    mxc_dma_cfg_adv.tosel = MXC_DMA_TIMEOUT_4_CLK;
+    mxc_dma_cfg_adv.pssel = MXC_DMA_PRESCALE_DISABLE;
+    mxc_dma_cfg_adv.burst_size = config->source_burst_length;
 
     /* DMA Transfer Config */
     mxc_dma_srcdst_t txfer;
@@ -109,28 +124,34 @@ static inline int max32_dma_config(const struct device *dev,
     txfer.len = config->head_block->block_size;
 
     /* Call required by SDK DMA driver, 
-     * but will use channel passed as argument */
+     * but we will use channel passed as argument */
     MXC_DMA_AcquireChannel(); 
 
-    ret = MXC_DMA_ConfigChannel(dma_cfg, txfer);
+    ret = MXC_DMA_ConfigChannel(mxc_dma_cfg, txfer);
     if (ret != E_NO_ERROR) {
 		return ret;
 	}
 
-    ret = MXC_DMA_AdvConfigChannel(dma_cfg_adv);
+    ret = MXC_DMA_AdvConfigChannel(mxc_dma_cfg_adv);
     if (ret) {
 		return ret;
 	}
 
+    /* Enable interrupts for the DMA peripheral */
     ret = MXC_DMA_EnableInt(channel); 
     if (ret != E_NO_ERROR) {
 		return ret;
 	}
 
-    ret = MXC_DMA_ChannelEnableInt(channel, MXC_F_DMA_CTRL_CTZ_IE);
+    /* Enable complete and count-to-zero interrupts for the channel */
+    ret = MXC_DMA_ChannelEnableInt(channel, MXC_F_DMA_CTRL_DIS_IE | MXC_F_DMA_CTRL_CTZ_IE);
     if (ret != E_NO_ERROR) {
 		return ret;
 	}
+
+    data[channel].callback = config->dma_callback;
+    data[channel].cb_data = config->user_data;
+
     return ret;
 }
 
@@ -147,29 +168,68 @@ int max32_dma_start(const struct device *dev, uint32_t channel)
 
 int max32_dma_stop(const struct device *dev, uint32_t channel)
 {
-    return MXC_DMA_ReleaseChannel(channel);
+    return MXC_DMA_Stop(channel);
 }
 
 static inline int max32_dma_get_status(const struct device *dev, 
                         uint32_t channel, struct dma_status *stat)
 {
+    const struct max32_dma_config *cfg = dev->config;
+
+    if (channel >= cfg->channels) {
+        LOG_ERR("Invalid DMA channel - must be < %" PRIu32 " (%" PRIu32 ")", cfg->channels, channel);
+        return -EINVAL;
+	}
+
+    int flags = 0;
+    flags = MXC_DMA_ChannelGetFlags(channel);
+
+    /* Channel is busy if no interrupt pending */
+    stat->busy = !(flags & MXC_F_DMA_STATUS_IPEND);
+
     return 0;
 }
 
 static void max32_dma_isr(const struct device *dev)
 {
-    printk("In max32_dma_isr\n");
+    const struct max32_dma_config *cfg = dev->config;
+    //struct max32_dma_data *data = c
+    mxc_dma_regs_t *regs = cfg->regs;
     int flags;
-    flags = MXC_DMA_ChannelGetFlags(0);
-    MXC_DMA_ChannelClearFlags(0, flags);
-    flags = MXC_DMA_ChannelGetFlags(1);
-    MXC_DMA_ChannelClearFlags(1, flags);
+    int status = 0;
+
+    for (int i = 0; i < cfg->channels; i++) {
+        struct max32_dma_data data = dma_data[i];;
+        flags = MXC_DMA_ChannelGetFlags(i);
+
+        /* Check if channel is in use, if not, move to next channel */
+        if (flags <= 0) {
+            continue;
+        }
+
+        /* Check for error interrupts */
+        if (flags & (MXC_F_DMA_STATUS_BUS_ERR | MXC_F_DMA_STATUS_TO_IF))
+        {
+            status = -EIO;
+        }
+
+        if (dma_data[i].callback) {
+            dma_data[i].callback(dev, dma_data[i].cb_data, i, status);
+        }
+
+        MXC_DMA_ChannelClearFlags(i, flags);
+
+        /* No need to check rest of the channels if no interrupt flags set */
+        if (regs->intfl == 0)
+            break;
+    }
 }
     
 #define MAX32_DMA_IRQ_CONNECT(n, inst)				\
         IRQ_CONNECT(DT_IRQ_BY_IDX(inst, n, irq),    \
                 DT_IRQ_BY_IDX(inst, n, priority),   \
-                max32_dma_isr, NULL, 0);            \
+                max32_dma_isr,                      \
+                DEVICE_DT_GET(inst), 0);       \
         irq_enable(DT_IRQ_BY_IDX(inst, n, irq));    \
 
 #define CONFIGURE_ALL_IRQS(n) LISTIFY(n, MAX32_DMA_IRQ_CONNECT, (), DT_NODELABEL(dma))
@@ -177,7 +237,7 @@ static void max32_dma_isr(const struct device *dev)
 static int max32_dma_init(const struct device *dev)
 {
     int ret = 0;
-	const struct max32_dma_config *const cfg = dev->config;
+	const struct max32_dma_config *cfg = dev->config;
 
 	if (!device_is_ready(cfg->clock)) {
 		return -ENODEV;
@@ -200,19 +260,19 @@ static const struct dma_driver_api max32_dma_driver_api = {
 	.start = max32_dma_start,
 	.stop = max32_dma_stop,
 	.get_status = max32_dma_get_status,
-	//.chan_filter = dma_max32_api_chan_filter,
 };
 
 static const struct max32_dma_config dma_cfg = { 
-    .regs = (mxc_dma_regs_t *)DT_INST_REG_ADDR(0), 
+    .regs = (mxc_dma_regs_t *)DT_REG_ADDR(DT_NODELABEL(dma)), 
     .clock = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(0)), 
     .perclk.bus = DT_INST_CLOCKS_CELL(0, offset), 
     .perclk.bit = DT_INST_CLOCKS_CELL(0, bit), 
+    .channels = DT_PROP(DT_NODELABEL(dma), dma_channels), 
 };
 
 DEVICE_DT_DEFINE(DT_NODELABEL(dma), 
             &max32_dma_init, 
-            NULL, NULL, 
+            NULL, &dma_data, 
             &dma_cfg, POST_KERNEL, 
             CONFIG_DMA_INIT_PRIORITY, 
             &max32_dma_driver_api);
