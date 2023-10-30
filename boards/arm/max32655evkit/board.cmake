0f1d1a36cb207617e0f97d899061fe5dcfb1df2d
Kevin Townsend <kevin.townsend@analog.com>
2023-10-30 11:18:54 +0100

    boards: arm: max32655evkit: Add JLink support
    Adds JLink as an optional runner, which can be selected with
    `west` via `--runner jlink`.
    
    OpenOCD is left as the default runner for debug and flash.
    
    Signed-off-by: Kevin Townsend <kevin.townsend@analog.com>

--- boards/arm/max32655evkit/board.cmake
+++ boards/arm/max32655evkit/board.cmake
@@ -3,5 +3,11 @@
 
 board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
 board_runner_args(openocd --cmd-pre-init "source [find target/max32655.cfg]")
+board_runner_args(jlink "--device=MAX32655" "--reset-after-load")
+
+# Make OpenOCD the default runner
+set_ifndef(BOARD_DEBUG_RUNNER openocd)
+set_ifndef(BOARD_FLASH_RUNNER openocd)
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
